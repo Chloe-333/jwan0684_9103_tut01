4e73145e5da2045e337c4b3ca59afd898f3ab1d3
Chloe-333 <jwan0684@uni.sydney.edu.au>
2026-05-08 15:34:58 +1000

    P4

--- wk 9/sketch.js
+++ wk 9/sketch.js
@@ -1,45 +1,107 @@
-function setup() {
-  createCanvas(400, 400);
-  background(0);
-  angleMode(DEGREES);
+class Head {
+  display(x, y, angle, diameter, colour) {
+    
+    // your transformations go here
+    translate(x, y);
+    rotate(angle);
+    scale(diameter);
+
+    // Circular outline
+    strokeWeight(0.01);
+    ellipse(0, 0, 1);
+
+    // Style for head
+    fill(20);
+    stroke(20);
+
+    // Eye
+    rect(-0.17, -0.33, 0.15, 0.15);
+    line(-0.26, -0.33, -0.17, -0.33);
+    line(-0.02, -0.18, -0.02, -0.09);
+
+    // Nose
+    rect(0.11, -0.48, 0.01, 0.59);
+    line(0.02, 0.11, 0.11, 0.11);
+
+    // Mouth
+    rect(0.07, 0.11, 0.03, 0.27);
+    rect(0.02, 0.22, 0.04, 0.01);
+
+    // Chin
+    rect(-0.05, 0.38, 0.11, 0.11);
+    line(-0.12, 0.38, -0.05, 0.38);
+
+
+  }
 }
-  
+let head = null;
+
+function setup() {
+  createCanvas(500, 500);
+  angleMode(DEGREES);
+
+  // Instantiate a single head object 
+  head = new Head();
+}
+
 function draw() {
-  // Set the background to black with an alpha value of 1,
-  // so that the dot drawn has a visible trail
-  background(0, 1);
-  
-  // Move the origin position to the centre of the canvas
-  translate(width / 2, height / 2);
+  background(225);
 
-  // Slowly rotate the co-ordinate system
-  // (effectively rotating the canvas around the origin)
-  // Try changing the number 1600 to see what patterns you can make
-  rotate(frameCount * 0.25); // rotate 0.25 degrees per frame
-  
-  // Calculate translateX and translate Y, 
-  // so that the dot follows an elliptical pattern
-  let translateX = sin(frameCount) * width / 8;
-  let translateY = cos(frameCount) * height / 4; 
-  
-  // Move the origin position again using translateX and translateY
-  translate(translateX, translateY);
-  
-  // Draw the dot by drawing a circle with diameter 5 
-  // at the current origin (0,0) position, which has 
-  // been manipulated by the two calls to translate() above
-  noStroke();
-  fill(255);
-  circle(0, 0, 5);
+  // Draw the head so that it:
+  // - moves from left to right across the canvas,
+  // - is centred vertically, 
+  // - and rotates and grows over time.
+  head.display(
+    (frameCount % width), // frameCount % width will make sure the shapre moves to the right and resetsonce it reaches the end of the frame
+    height / 2, // Stays at the vertical centre of the canvas
+    frameCount, // rotate 1 degree every frame
+    (frameCount % width) / width * 200, // alternative to map((frameCount % width), 0, width, 0, 200);
+    color(245));
+}
 
-  // Draw another dot orbiting the first dot
-  // You can adjust the following value to change the speed of the second dot
-  let angle = frameCount * 5; // move 5 degrees every frame
-  let orbitDistance = 5; // Distance from the first dot
-  let orbitX = cos(angle) * orbitDistance;
-  let orbitY = sin(angle) * orbitDistance;
+// Bauhaus Head Class
+// The class has no constructor and a single display() method
+// used to draw the Bauhaus emblem
+class Head {
+  display(x, y, angle, diameter, colour) {
+    // The push() function saves any transformations and style settings
+    // that might proceed a call to this method
+    push();
 
-  translate(orbitX, orbitY);
-  fill(255, 0, 0); // Make the second dot red
-  circle(0, 0, 3); // And slightly smaller than the first dot
+    fill(colour);
+
+    // Perform all transforms in the order: translate, rotate and scale
+    translate(x, y);
+    rotate(angle);
+    scale(diameter);
+    
+    // Circular outline
+    strokeWeight(0.01);
+    ellipse(0, 0, 1);
+
+    // Style for head
+    fill(20);
+    stroke(20);
+
+    // Eye
+    rect(-0.17, -0.33, 0.15, 0.15);
+    line(-0.26, -0.33, -0.17, -0.33);
+    line(-0.02, -0.18, -0.02, -0.09);
+
+    // Nose
+    rect(0.11, -0.48, 0.01, 0.59);
+    line(0.02, 0.11, 0.11, 0.11);
+
+    // Mouth
+    rect(0.07, 0.11, 0.03, 0.27);
+    rect(0.02, 0.22, 0.04, 0.01);
+
+    // Chin
+    rect(-0.05, 0.38, 0.11, 0.11);
+    line(-0.12, 0.38, -0.05, 0.38);
+
+    // The pop() function restores any transformations and style settings
+    // that were saved by the earlier call to push()
+    pop();
+  }
 }
