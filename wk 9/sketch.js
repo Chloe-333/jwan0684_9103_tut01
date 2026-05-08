312fa4c3043ca8c584c097d82eb8a47d94145541
Chloe-333 <jwan0684@uni.sydney.edu.au>
2026-05-08 14:37:51 +1000

    transfom-part1

--- wk 9/sketch.js
+++ wk 9/sketch.js
@@ -1,44 +1,29 @@
-// We need variables to store the current square size between frames
-// as well as the target square size based on a random value calculated
-// every 120 frames
-let squareSize;
-let targetSquareSize;
-
-// Set the easing value to a constant
-// Try changing this value to see how it affects the transition
-const easing = 0.05;
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(230);
-
-  // We want the squares drawn to fade over time, so use a semi-transparent fill
-  fill(20, 20); 
-  stroke(170);
-  
-  // We want to draw the square in the middle of the canvas, 
-  // which is easier to achieve using rectMode(CENTER)
-  rectMode(CENTER); 
-
-  // Start with a random square size as the nominated current size
-  squareSize = random(height);
-
-  // The initial target square size is set to half the current square size
-  targetSquareSize = squareSize / 2;
+  createCanvas(400, 400);
+  background(0);
+  angleMode(DEGREES);
 }
-
+  
 function draw() {
-  // Every 120 frames (or 2 seconds assuming the sketch is running at 
-  // 60 frames per second) a new target size is calculated at random
-  if (frameCount % 120 == 0) {
-    targetSquareSize = random(height / 2);
-  }
-
-  // The new square size is calculated by linearly interpolating between the 
-  // last square size drawn and the target square size that gets updated
-  // every 120 frames
-  squareSize = lerp(squareSize, targetSquareSize, easing);
-
-  // Draw the square using the updated size in the middle of the canvas
-  square(width / 2, height / 2, squareSize);
+  // Set the background to black with an alpha value of 1,
+  // so that the dot drawn has a visible trail
+  background(0, 1);
+  
+  // Move the origin position to the centre of the canvas
+  translate(width / 2, width / 2);
+  
+  // Calculate translateX and translate Y, 
+  // so that the dot follows an elliptical pattern
+  let translateX = sin(frameCount) * width / 8;
+  let translateY = cos(frameCount) * height / 4; 
+  
+  // Move the origin position again using translateX and translateY
+  translate(translateX, translateY);
+  
+  // Draw the dot by drawing a circle with diameter 5
+  // at the current origin (0,0) position, which has 
+  // been manipulated by the two calls to translate() above
+  noStroke();
+  fill(255);
+  circle(0, 0, 5);
 }
