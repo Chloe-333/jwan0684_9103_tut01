725409736f8d9ea4c017e18b605aa8dad09499c2
Chloe-333 <jwan0684@uni.sydney.edu.au>
2026-05-08 14:49:03 +1000

    P2

--- wk 9/sketch.js
+++ wk 9/sketch.js
@@ -10,7 +10,12 @@ function draw() {
   background(0, 1);
   
   // Move the origin position to the centre of the canvas
-  translate(width / 2, width / 2);
+  translate(width / 2, height / 2);
+
+  // Slowly rotate the co-ordinate system
+  // (effectively rotating the canvas around the origin)
+  // Try changing the number 0.25 to see what patterns you can make
+  rotate(frameCount * 0.25); // move 0.25 degrees per frame
   
   // Calculate translateX and translate Y, 
   // so that the dot follows an elliptical pattern
@@ -20,7 +25,7 @@ function draw() {
   // Move the origin position again using translateX and translateY
   translate(translateX, translateY);
   
-  // Draw the dot by drawing a circle with diameter 5
+  // Draw the dot by drawing a circle with diameter 5 
   // at the current origin (0,0) position, which has 
   // been manipulated by the two calls to translate() above
   noStroke();
