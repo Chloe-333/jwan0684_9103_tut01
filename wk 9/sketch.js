704aa88554d0998575c945293da3b66abc158fbf
Chloe-333 <jwan0684@uni.sydney.edu.au>
2026-05-15 14:10:24 +1000

    Part 1

--- wk 9/sketch.js
+++ wk 9/sketch.js
@@ -1,125 +1,60 @@
-//We need a variable to hold our image
-let img;
+// We will make some arrays to hold different kinds of random numbers.
+let randomNumberArray = [];
+let perlinNoiseArray = [];
 
-//We will divide the image into segments
-let numSegments = 50;
+// We will define the length of the array here so we can change it easily.
+// We will use this in our for loops.
+let valueArrayLength = 50;
 
-//We will store the segments in an array
-let segments = [];
-
-//let's add a variable to switch between drawing the image and the segments
-let drawSegments = false;
-
-//Let's make an object to hold the draw properties of the image
-let imgDrwPrps = {aspect: 0, width: 0, height: 0, xOffset: 0, yOffset: 0};
-
-//And a variable for the canvas aspect ratio
-let canvasAspectRatio = 0;
-
-//let's load the image from disk
-function preload() {
-  img = loadImage('/assets/Mona_Lisa_by_Leonardo_da_Vinci_500_x_700.jpg');
-}
+// And now a variable to control how close together the perlin noise values are taken from
+// the noise function.
+// Try to change this and watch what happens to the noise values.
+// It is like zooming in and out of the noise function.
+let perlinNoiseStep = 0.1;
 
 function setup() {
-  //We will make the canvas the same size as the image using its properties
-  createCanvas(windowWidth, windowHeight);
-  //let's calculate the aspect ratio of the image
-  imgDrwPrps.aspect = img.width / img.height;
-  //and the aspect ratio of the canvas
-  canvasAspectRatio = width / height;
-  calculateImageDrawProps();
-  //We can use the width and height of the image to calculate the size of each segment
-  let segmentWidth = img.width / numSegments;
-  let segmentHeight = img.height / numSegments;
-  /*
-  Divide the original image into segments, we are going to use nested loops
-  */
+  createCanvas(1000, 500);
 
-  for (let segYPos=0; segYPos<img.height; segYPos+=segmentHeight) {
-    //this is looping over the height
-    for (let segXPos=0; segXPos<img.width; segXPos+=segmentWidth) {
-      //We will use the x and y position to get the colour of the pixel from the image
-      //let's take it from the centre of the segment
-      let segmentColour = img.get(segXPos + segmentWidth / 2, segYPos + segmentHeight / 2);
-       let segment = new ImageSegment(segXPos,segYPos,segmentWidth,segmentHeight,segmentColour);
-       segments.push(segment);
-    }
+  // We will fill our array with random numbers between 0 and 1.
+  for (let i = 0; i < valueArrayLength; i++) {
+    randomNumberArray.push(random(0, 1));
   }
+
+  // Perlin noise, through the noise function, creates a smooth gradient of values, kind of
+  // like a waveform.
+  // Now let's fill our perlin noise array with values close by each other from the noise
+  // function.
+  for (let i = 0; i < valueArrayLength; i += perlinNoiseStep) {
+    perlinNoiseArray.push(noise(i));
+  }
+
+  // We will draw some text so we set the font size once here because it will be the same
+  // for all text.
+  textSize(20);
 }
 
 function draw() {
-  background(0);
-  if (drawSegments) {
-    //let's draw the segments to the canvas
-    for (const segment of segments) {
-      segment.draw();
-    }
-  } else {
-    //let's draw the image to the canvas
-    image(img, imgDrwPrps.xOffset, imgDrwPrps.yOffset, imgDrwPrps.width, imgDrwPrps.height);
-  }
-}
-function keyPressed() {
-  if (key == " ") {
-    //this is a neat trick to invert a boolean variable,
-    //it will always make it the opposite of what it was
-    drawSegments = !drawSegments;
-  }
+  // Set up our drawing environment
+  background(255);
+  noStroke();
+  fill(0);
+
+  drawValues("Random values", 50, 70, randomNumberArray);
+  drawValues("Perlin noise values", 50, 280, perlinNoiseArray);
 }
 
+// A function to draw our array values as rectangles where the height is derived from each
+// value of the array.
+// This function takes a title, an x and y position, and an array of values to draw.
+function drawValues(title, x, y, randomOrNoiseArray) {
+  // Let's draw a text title
+  text(title, x, y);
 
-function calculateImageDrawProps() {
-   /**
-   ASPECT Ratio:
-   * If > 1: image is wider than it is tall (landscape)
-   * If < 1: image is taller than it is wide (portrait)
-   * If = 1: image is square
-   **/ 
-   
-  //if the image is wider than the canvas
-  if (imgDrwPrps.aspect > canvasAspectRatio) {
-    //then we will draw the image to the width of the canvas
-    imgDrwPrps.width = width;
-    //and calculate the height based on the aspect ratio
-    imgDrwPrps.height = width / imgDrwPrps.aspect;
-    imgDrwPrps.yOffset = (height - imgDrwPrps.height) / 2;
-    imgDrwPrps.xOffset = 0;
-    
-    // Image is taller than the canvas
-  } else if (imgDrwPrps.aspect < canvasAspectRatio) {
-    //otherwise we will draw the image to the height of the canvas
-    imgDrwPrps.height = height;
-    //and calculate the width based on the aspect ratio
-    imgDrwPrps.width = height * imgDrwPrps.aspect;
-    imgDrwPrps.xOffset = (width - imgDrwPrps.width) / 2;
-    imgDrwPrps.yOffset = 0;
-  }
-  else if (imgDrwPrps.aspect == canvasAspectRatio) {
-    //if the aspect ratios are the same then we can draw the image to the canvas size
-    imgDrwPrps.width = width;
-    imgDrwPrps.height = height;
-    imgDrwPrps.xOffset = 0;
-    imgDrwPrps.yOffset = 0;
-  }
-}
-//Here is our class for the image segments, we start with the class keyword
-class ImageSegment {
-
-  constructor(srcImgSegXPosInPrm,srcImgSegYPosInPrm,srcImgSegWidthInPrm,srcImgSegHeightInPrm,srcImgSegColourInPrm) {
-    //these parameters are used to set the internal properties of an instance of the segment
-    //These parameters are named as imageSource as they are derived from the image we are using
-    this.srcImgSegXPos = srcImgSegXPosInPrm;
-    this.srcImgSegYPos = srcImgSegYPosInPrm;
-    this.srcImgSegWidth = srcImgSegWidthInPrm;
-    this.srcImgSegHeight = srcImgSegHeightInPrm;
-    this.srcImgSegColour = srcImgSegColourInPrm;
-  }
-
-  draw() {
-    //let's draw the segment to the canvas, for now we will draw it as an empty rectangle so we can see it
-    stroke(0);
-    fill(this.srcImgSegColour);
-    rect(this.srcImgSegXPos, this.srcImgSegYPos, this.srcImgSegWidth, this.srcImgSegHeight);
+  // We will make all the rectangles fit into 200 pixels so the width of each rectangle 
+  // will be calculated as 200 divided by the number of values in the array.
+  // Note the negative sign in the height calculation - this is because the y-axis is 
+  // inverted in p5.js so we need to multiply by -1 to make the rectangles draw upwards
+  for (let i = 0; i < valueArrayLength; i++) {
+    rect(x + (i * 200) / valueArrayLength, y + 130, 200 / valueArrayLength, randomOrNoiseArray[i] * -100);
   }
 }
